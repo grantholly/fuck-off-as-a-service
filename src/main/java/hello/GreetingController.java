f20d1a3f4f8aafe67141ec3bab6f2175ecbb7516
grantholly <gdholly@gmail.com>
2016-08-29 22:00:54 -0700

    first controller for /greeting endpoint

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,19 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Greeting Controller {
+    public static final String template = "Fuck off, %s!";
+    public final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name",
+					   defaultValue="you") String name) {
+	return new Greeting(counter.incrementAndGet(),
+			    String.format(tamplate, name));
+    }
+}
